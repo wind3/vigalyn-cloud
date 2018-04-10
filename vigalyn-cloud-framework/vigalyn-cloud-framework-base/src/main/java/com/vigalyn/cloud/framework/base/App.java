ea352d53003d6648ecb4bb2a9693311716cf70c5
曾维嘉 <zengwj@cmrh.com>
2018-04-10 14:55:16 +0800

    增加目录结构

---
+++ vigalyn-cloud-framework/vigalyn-cloud-framework-base/src/main/java/com/vigalyn/cloud/framework/base/App.java
@@ -0,0 +1,13 @@
+package com.vigalyn.cloud.framework.base;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
